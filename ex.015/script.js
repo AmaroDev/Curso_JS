b5ce248b6acfb727159cbb55be82ac6cbdda26c9
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2024-03-01 18:44:05 -0300

    continua

--- ex.015/script.js
+++ ex.015/script.js
@@ -1,23 +1,14 @@
-function carregar() {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 11
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if(hora >= 0 && hora <= 12) {
-        //BOM DIA
-        img.src = 'imagens/manhã.png'
-        document.body.style.body = '#fbce86'
-    } else if (hora >= 12 && hora <= 18){
-        //BOA TARDE
-        img.src = 'imagens/tarde.png'
-        document.body.style.body = '#92a5b9'
-    } else {
-        //BOA NOITE
-        img.src = 'imagens/noite.png'
-        document.body.style.body = '#fd8f95'
+function verificar() {
+    var data = new Date() //
+    var ano = data.getFullYear() // Pega a data com os 4 Dígitos//
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length ==0 || fano.value > ano) {
+        alert('[ERRO] verifique os dados e tente novamente!')
+    }else {
+        alert('tudo ok')
     }
+   
 
 
 }
