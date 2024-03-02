fe795509b4f9e126a0582ae5226586f1ffcb1b84
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2024-03-02 20:29:38 -0300

    continua

--- ex.015/script.js
+++ ex.015/script.js
@@ -6,7 +6,19 @@ function verificar() {
     if (fano.value.length ==0 || fano.value > ano) {
         alert('[ERRO] verifique os dados e tente novamente!')
     }else {
-        alert('tudo ok')
+        var fsex= document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        //res.innerHTML= `Idade calculada: ${idade}`//
+        var  gênero = ''
+        if (fsex[0].checked) {
+            gênero  = 'homem'
+
+        }else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        
     }
    
 
