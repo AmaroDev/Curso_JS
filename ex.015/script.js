3713057271c4ac2a6528d9bd6a5ee5bb96764824
Amaro151723 <148920071+AmaroDev@users.noreply.github.com>
2024-03-04 20:46:54 -0300

    concluido... Exercício importante

--- ex.015/script.js
+++ ex.015/script.js
@@ -16,24 +16,37 @@ function verificar() {
             gênero  = 'homem'
             if(idade >=0 && idade <10) {
                 // Criança
+                img.setAttribute('src', 'meninobb.png')
             } else if  (idade < 21) {
                 //Adulto
+                img.setAttribute('src', 'adolescenteG.png')
+            } else if  (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homemadulto.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'idoso.png')
             }
 
         }else if (fsex[1].checked) {
             gênero = 'Mulher'
             if(idade >=0 && idade <10) {
                 // Criança
+                img.setAttribute('src', 'meninabb.png')
             } else if  (idade < 21) {
                 //Adulto
+                img.setAttribute('src', 'adolescenteM.png')
+            } else if  (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulheadulta.png')
             } else {
                 //Idoso
+                img.setAttribute('src', 'idosa.png')
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.innerHTML = `Detectamos ${gênero} com ${idade } anos. `
+        res.appendChild(img) 
         
     }
    
